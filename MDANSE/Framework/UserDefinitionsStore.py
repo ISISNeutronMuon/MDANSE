9e4f94fad2c3125007a7f7671dead5facb05f2f7
Eric Pellegrini <pellegrini@ill.fr>
2015-07-02 16:53:56 +0200

    Simplified the user definition store by removing the need of IUserDefinitions interface when setting an ud

--- MDANSE/Framework/UserDefinitionsStore.py
+++ MDANSE/Framework/UserDefinitionsStore.py
@@ -4,7 +4,6 @@ import os
 from MDANSE import PLATFORM
 from MDANSE.Core.Error import Error
 from MDANSE.Core.Singleton import Singleton
-from MDANSE.Framework.UserDefinitions.IUserDefinition import IUserDefinition
 
 class UserDefinitionsStoreError(Error):
     pass
@@ -76,19 +75,13 @@ class UserDefinitionsStore(dict):
         Returns a user definition given its target, category and its name.
                 
         :return: the user definition if it found or None otherwise
-        :rtype: a MDANSE.Framework.UserDefinitions.IUserDefinition.IUserDefinition derived class
+        :rtype: any
         '''
             
         if not self.has_definition(target,section,name):
             raise UserDefinitionsStoreError('The item %r could not be found' % (target,section,name))
 
         ud = self[target][section][name]
-                        
-        if ud.target != target:
-            raise UserDefinitionsStoreError('Target mismatch between %r and %r' % (target,ud.target))
-    
-        if ud.type != section:
-            raise UserDefinitionsStoreError('Type mismatch between %r and %r' % (section,ud.type))
             
         return ud
         
@@ -98,10 +91,7 @@ class UserDefinitionsStore(dict):
             del self[target][section][name]
                                                    
     def set_definition(self, target, section, name, value):
-                
-        if not isinstance(value,IUserDefinition):
-            raise UserDefinitionsStore('The input value is not a User definition.')
-        
+                        
         if self.has_definition(target, section, name):
             raise UserDefinitionsStoreError('Item %s is already registered as an user definition. You must delete it before setting it.' % (target,section,name))
 
