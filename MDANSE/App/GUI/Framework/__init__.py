7adb267c2e59d5fbb4839372f3a6e4d6dd9a6c4d
Eric Pellegrini <pellegrini@ill.fr>
2015-07-23 17:10:38 +0200

    Removed unused packages/modules

--- MDANSE/App/GUI/Framework/__init__.py
+++
@@ -1,9 +0,0 @@
-def has_parent(window, target):
-        
-    if window == target:
-        return True
-
-    if window.TopLevelParent == window:
-        return False
-
-    return has_parent(window.Parent, target)    
