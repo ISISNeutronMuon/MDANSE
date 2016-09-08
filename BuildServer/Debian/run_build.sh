211a86739a79a0c6ba8f954462dcc44147278190
Eric Pellegrini <pellegrini@ill.fr>
2016-09-08 11:47:38 +0200

    Cosmetic changes in run_build script for trusty

--- BuildServer/Debian/run_build.sh
+++ BuildServer/Debian/run_build.sh
@@ -8,15 +8,8 @@ export DISTRO=$2
 #############################
 
 ## Add some colors
-VERT="\\033[1;32m"
-NORMAL="\\033[0;39m"
 ROUGE="\\033[1;31m"
-ROSE="\\033[1;35m"
 BLEU="\\033[1;34m"
-BLANC="\\033[0;02m"
-BLANCLAIR="\\033[1;08m"
-JAUNE="\\033[1;33m"
-CYAN="\\033[1;36m"
 
 if [ -n "${RUN_NIGHTLY_BUILD}" ]
 then
