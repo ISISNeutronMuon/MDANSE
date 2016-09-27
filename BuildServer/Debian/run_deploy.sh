a2309d4e58d675a5d7d5945c5ad00bf9ee31c379
Eric Pellegrini <pellegrini@ill.fr>
2016-09-27 10:29:35 +0200

    advances in gitlab-gitflow integration

--- BuildServer/Debian/run_deploy.sh
+++ BuildServer/Debian/run_deploy.sh
@@ -11,7 +11,7 @@ export DISTRO=$2
 ROUGE="\\033[1;31m"
 BLEU="\\033[1;34m"
 
-VERSION_NAME=`sed -n "sed -n 's/__version__.*=.*\"\(.*\)\"/\1/p' __pkginfo__.py" MDANSE/__pkginfo__.py`
+VERSION_NAME=`sed -n "sed -n 's/__version__.*=.*\"\(.*\)\"/\1/p' MDANSE/__pkginfo__.py`
 if [ -n "${CI_BUILD_REF_NAME}" ]
 then
     VERSION_NAME=${VERSION_NAME}-${CI_BUILD_REF}
