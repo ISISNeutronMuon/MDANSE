641033bd6139ecbbbce6cd1223a196899638c004
Eric Pellegrini <pellegrini@ill.fr>
2016-09-27 09:44:13 +0200

    advances in gitlab-gitflow integration

--- BuildServer/Debian/run_tests.sh
+++ BuildServer/Debian/run_tests.sh
@@ -6,13 +6,13 @@ export PYTHONPATH=${CI_PROJECT_DIR}/build/lib.linux-x86_64-2.7
 
 # Performs the unit tests
 cd Tests/UnitTests
-test=`nosetests --verbosity=3 -P .`
-echo "test=$test"
+nosetests --verbosity=3 -P .
+echo "test=$?"
 cd ../..
 
 # Performs the functional tests
 cd Tests/FunctionalTests/Jobs
 python BuildJobTests.py
-test=`nosetests --verbosity=3 --exe -P .`
-echo "test1=$test"
+nosetests --verbosity=3 --exe -P .
+echo "test=$?"
 
