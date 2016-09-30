8f485c687036f5a3b2cb4cbc3a5de4bd5562669c
Jamie Hall <hall@ill.eu>
2016-09-30 18:16:29 +0200

    Update run_deploy.sh

--- BuildServer/Debian/run_deploy.sh
+++ BuildServer/Debian/run_deploy.sh
@@ -13,13 +13,14 @@ BLEU="\\033[1;34m"
 
 VERSION_NAME=`sed -n 's/__version__.*=.*\"\(.*\)\"/\1/p' MDANSE/__pkginfo__.py`
 
-echo "dfjsdhfdshfjkhfkjxhfxj" ${CI_BUILD_REF_NAME}
-if [[ ${CI_BUILD_REF_NAME} =~ feature- ]]
+if [[ ${CI_BUILD_REF_NAME} =~ develop ]]
 then
-    VERSION_NAME=${VERSION_NAME}-`git rev-parse --short HEAD`
-elif [ -n "${WEEKLY_BUILD}" ]
-then
-    VERSION_NAME=${VERSION_NAME}-"weekly-build-"`date +%Y-%m-%d`
+    if [ -n "${WEEKLY_BUILD}" ]
+    then
+        VERSION_NAME=${VERSION_NAME}-"weekly-build-"`date +%Y-%m-%d`
+    else
+        VERSION_NAME=${VERSION_NAME}-`git rev-parse --short HEAD`
+    fi
 fi
 
 ##Select the build target
