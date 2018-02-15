a2772996ba490836dae52f4b332b9c66e8d74aa6
Eric Pellegrini <pellegrini@ill.fr>
2018-02-15 18:24:20 +0100

    debug windows build script

--- BuildServer/Windows/run_build.sh
+++ BuildServer/Windows/run_build.sh
@@ -177,7 +177,7 @@ mv \$_OUTDIR/bin/netcdf.dll .
 mv \$_OUTDIR/include/netcdf.h .
 mv \$_OUTDIR/lib/netcdf.lib .
 
-# rm -rf \$_OUTDIR
+rm -rf \$_OUTDIR
 
 git clone https://code.ill.fr/scientific-software/scientific-python.git
 cd scientific-python
@@ -192,7 +192,6 @@ rm netcdf.lib
 
 git clone https://code.ill.fr/scientific-software/mmtk.git
 cd mmtk
-git checkout master
 cmd /V:ON /E:ON /C "${SCRIPT_DIR_WIN}/setup_and_build.bat" "${DEPENDENCIES_DIR_WIN}/mmtk" "${TARGET_DIR}" "${MSVC_BUILD_TARGET}"
 cd ..
 rm -rf mmtk
