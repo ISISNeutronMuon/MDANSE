673989f7ebab8dcb11e2330d28cc31afa9dbf6f3
Eric Pellegrini <pellegrini@ill.fr>
2018-05-30 14:06:59 +0200

    Trying to figure why the artefacts are not seen by gitlab once uploaded

--- BuildServer/Docker/upload_artefacts.sh
+++ BuildServer/Docker/upload_artefacts.sh
@@ -1,5 +1,4 @@
 docker run --rm -iv${PWD}:/artifacts/ deploy_mdanse_xenial << COMMANDS
-chown ci:ci /mdanse/BuildServer/*.deb
 chmod 755 /mdanse/BuildServer/*.deb
 cp -a /mdanse/BuildServer/*.deb /artifacts/.
 COMMANDS
