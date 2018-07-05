9f36c1ad6d6a61ef93a199b531d7135008c3a59a
Eric Pellegrini <pellegrini@ill.fr>
2018-07-05 17:26:06 +0200

    Fix bug when running setup_ci script

--- BuildServer/Unix/setup_ci.sh
+++ BuildServer/Unix/setup_ci.sh
@@ -12,8 +12,6 @@ export CI_TEMP_INSTALL_DIR=${CI_PROJECT_DIR}/temp/install
 
 export PYTHONPATH=${CI_TEMP_INSTALL_DIR}/lib/python2.7/site-packages:${PYTHONPATH}
 
-rm -rf ${CI_TEMP_DIR}
-
 mkdir -p ${CI_TEMP_DIR}
 
 cd ${CI_PROJECT_DIR}
@@ -35,28 +33,6 @@ export RED="\\033[1;31m"
 export BLUE="\\033[1;34m"
 export NORMAL="\\033[0m"
 
-# Update the __pkginfo__ file with the current commit
 echo -e "${BLUE}""Commit id = ${CI_COMMIT_ID}""${NORMAL}"
 echo -e "${BLUE}""Branch name = ${CI_COMMIT_REF_NAME}""${NORMAL}"
-$SED_I_COMMAND "s/.*__commit__.*/__commit__ = \"${CI_COMMIT_ID}\"/" ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py
 
-# Get MDANSE version
-MDANSE_VERSION=`sed -n 's/__version__.*=.*\"\(.*\)\"/\1/p' ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py`
-
-# Check if branch is master
-if [[ ${CI_COMMIT_REF_NAME} == "master" ]]
-then
-    VERSION_NAME=${MDANSE_VERSION}
-    ${SED_I_COMMAND} "s/.*__beta__.*/__beta__ = None/" ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py
-else
-    # Check if branch is release*
-	if [[ ${CI_COMMIT_REF_NAME::7} == "release" ]]
-	then
-	    VERSION_NAME=${MDANSE_VERSION}-rc-${CI_COMMIT_ID}
-	    ${SED_I_COMMAND} "s/.*__beta__.*/__beta__ = \"rc\"/" ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py
-	else
-	    VERSION_NAME=${MDANSE_VERSION}-beta-${CI_COMMIT_ID}
-	    ${SED_I_COMMAND} "s/.*__beta__.*/__beta__ = \"beta\"/" ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py
-	fi
-fi
-export VERSION_NAME
