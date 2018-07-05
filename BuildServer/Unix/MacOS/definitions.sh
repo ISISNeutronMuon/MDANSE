496358685c9ffe27d23eceed5e7dfb79ef0804d7
Eric Pellegrini <pellegrini@ill.fr>
2018-07-05 14:23:31 +0200

    Fixing the MacOS failing ci

--- BuildServer/Unix/MacOS/definitions.sh
+++ BuildServer/Unix/MacOS/definitions.sh
@@ -7,5 +7,3 @@ export PYTHONEXE=/usr/bin/python
 # The sed -i"" is compulsory other crashes on macos
 export SED_I_COMMAND='sed -i ""'
 
-export MDANSE_DEPENDENCIES_DIR=/Users/ci/Projects/mdanse/dependencies
-
--- BuildServer/Unix/version.sh
+++ BuildServer/Unix/version.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-cd $MDANSE_SOURCE_DIR
+cd ${CI_PROJECT_DIR}
 
 # Update the __pkginfo__ file with the current commit
 echo -e "${BLUE}""Commit id = ${MDANSE_GIT_CURRENT_COMMIT}<--""${NORMAL}"
 echo -e "${BLUE}""Branch name = ${MDANSE_GIT_BRANCH_NAME}<--""${NORMAL}"
-$SED_I_COMMAND "s/.*__commit__.*/__commit__ = \"${MDANSE_GIT_CURRENT_COMMIT}\"/" MDANSE/__pkginfo__.py
+$SED_I_COMMAND "s/.*__commit__.*/__commit__ = \"${MDANSE_GIT_CURRENT_COMMIT}\"/" ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py
 
 # Get MDANSE version
-MDANSE_VERSION=`sed -n 's/__version__.*=.*\"\(.*\)\"/\1/p' MDANSE/__pkginfo__.py`
+MDANSE_VERSION=`sed -n 's/__version__.*=.*\"\(.*\)\"/\1/p' ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py`
 
 # Check if branch is master
 if [[ ${MDANSE_GIT_BRANCH_NAME} == "master" ]]
 then
     VERSION_NAME=${MDANSE_VERSION}
-    $SED_I_COMMAND "s/.*__beta__.*/__beta__ = None/" MDANSE/__pkginfo__.py
+    $SED_I_COMMAND "s/.*__beta__.*/__beta__ = None/" ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py
 else
     # Check if branch is release*
 	if [[ ${MDANSE_GIT_BRANCH_NAME::7} == "release" ]]
 	then
 	    VERSION_NAME=${MDANSE_VERSION}-rc-${MDANSE_GIT_CURRENT_COMMIT}
-	    $SED_I_COMMAND "s/.*__beta__.*/__beta__ = \"rc\"/" MDANSE/__pkginfo__.py
+	    $SED_I_COMMAND "s/.*__beta__.*/__beta__ = \"rc\"/" ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py
 	else
 	    VERSION_NAME=${MDANSE_VERSION}-beta-${MDANSE_GIT_CURRENT_COMMIT}
-	    $SED_I_COMMAND "s/.*__beta__.*/__beta__ = \"beta\"/" MDANSE/__pkginfo__.py
+	    $SED_I_COMMAND "s/.*__beta__.*/__beta__ = \"beta\"/" ${CI_PROJECT_DIR}/MDANSE/__pkginfo__.py
 	fi
 fi
 export VERSION_NAME
