27aeb7a52daf157445da50d575a2c69a03621d71
Perenon R <perenon@ill.fr>
2020-02-26 15:52:01 +0100

    Using Gitlab env. variables to set commit nb and branch name on Windows

--- BuildServer/Windows/setup_ci.bat
+++ BuildServer/Windows/setup_ci.bat
@@ -1,8 +1,24 @@
 @echo off
 
-cd "%MDANSE_SOURCE_DIR%"
+rem Set the source directory
+set MDANSE_SOURCE_DIR=%cd%
 
+rem Set th directory where the MDANSE dependencies are stored
+set MDANSE_DEPENDENCIES_DIR=C:\\Projects\\mdanse\\resources\\dependencies\\%BUILD_TARGET%
+
+rem Set the location where the temporary Python will be installed
+set MDANSE_TEMPORARY_INSTALLATION_DIR=%MDANSE_SOURCE_DIR%\\BuildServer\\Windows\\Build
+
+rem Get revision number from Git
 rem To understand this syntax "set cmd=...... for /F %%ii ......", see https://stackoverflow.com/questions/2323292/assign-output-of-a-program-to-a-variable
+rem set cmd="git rev-parse --short HEAD"
+rem for /F %%i in (' %cmd% ') do set MDANSE_GIT_CURRENT_COMMIT=%%i
+set MDANSE_GIT_CURRENT_COMMIT=%CI_COMMIT_SHORT_SHA%
+
+rem Get commit branch from Gitlab
+set MDANSE_GIT_BRANCH_NAME=%CI_COMMIT_REF_NAME%
+
+cd "%MDANSE_SOURCE_DIR%"
 
 rem Update the __pkginfo__ file with the current commit
 echo "Commit id %MDANSE_GIT_CURRENT_COMMIT%"
