f21faf4ffab1ac571e744ff40c7de51d949a04ef
Turanyi <rastislav.turanyi@stfc.ac.uk>
2021-10-18 16:30:47 +0100

    Add investigation to osx deploy.sh

--- BuildServer/Unix/MacOS/deploy.sh
+++ BuildServer/Unix/MacOS/deploy.sh
@@ -17,10 +17,10 @@ export MDANSE_APP_DIR=${CI_TEMP_DIR}/dist/MDANSE.app
 export PYTHONPATH=$HOME/Python/lib/python2.7/site-packages:${PYTHONPATH}
 
 export PYTHONEXE=$HOME/$RUNNER_TOOL_CACHE/Python/2.7.18/x64/bin/python
-
+echo "Build mdanse"
 # Build API
 sudo $PYTHONEXE setup.py build build_api build_help install
-
+echo "Finished building mdanse"
 status=$?
 if [ $status -ne 0 ]; then
 	echo -e "${RED}" "Failed to build MDANSE Documentation""${NORMAL}"
@@ -36,7 +36,7 @@ echo -e "${BLUE}""Packaging MDANSE""${NORMAL}"
 mkdir ${CI_TEMP_DIR}/dist
 mkdir ${CI_TEMP_DIR}/dist/MDANSE.app
 cp -R ${MDANSE_DEPENDENCIES_DIR} ${MDANSE_APP_DIR}/Contents
-cp -R $HOME/Python/lib/python2.7/site-packages/* ${MDANSE_APP_DIR}/Contents/Resources/lib/python2.7/
+cp -R $HOME/python/lib/python2.7/site-packages/* ${MDANSE_APP_DIR}/Contents/Resources/lib/python2.7/
 
 # Add MDANSE version file (should read the version from the bundle with pyobjc, but will figure that out later)
 echo "${VERSION_NAME}" > ${MDANSE_APP_DIR}/Contents/Resources/version
@@ -47,10 +47,10 @@ cp $GITHUB_WORKSPACE/Scripts/mdanse_gui ${MDANSE_APP_DIR}/Contents/Resources/
 # Modify Info.plist and copy it
 sed -i "" "s/<MDANSE_VERSION>/${VERSION_NAME}/" $GITHUB_WORKSPACE/BuildServer/Unix/MacOS/Resources/Info.plist
 cp $GITHUB_WORKSPACE/BuildServer/Unix/MacOS/Resources/Info.plist ${MDANSE_APP_DIR}/Contents/
-
+echo "Starting installation"
 # Relink netcdf
 install_name_tool -change /usr/local/opt/netcdf/lib/libnetcdf.18.dylib @executable_path/../Frameworks/libnetcdf.18.dylib ${MDANSE_APP_DIR}/Contents/Resources/lib/python2.7/Scientific/_netcdf.so
-
+echo "start dmg creation"
 #############################
 # Create DMG
 #############################
