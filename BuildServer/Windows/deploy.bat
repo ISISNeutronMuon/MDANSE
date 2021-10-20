97e9b06e075c50b5b188565f803e824fa1f307c4
Turanyi <rastislav.turanyi@stfc.ac.uk>
2021-10-20 11:20:01 +0100

    Change env variables in deploy.bat

--- BuildServer/Windows/deploy.bat
+++ BuildServer/Windows/deploy.bat
@@ -2,7 +2,7 @@
 
 cd %MDANSE_SOURCE_DIR%
 
-set MDANSE_TEMPORARY_INSTALLATION_DIR=%GITHUB_WORKSPACE%\BuildServer\Windows\Build
+set MDANSE_TEMPORARY_INSTALLATION_DIR="%GITHUB_WORKSPACE%\BuildServer\Windows\Build"
 rem Set the path to python executable
 set PYTHON_EXE=%MDANSE_TEMPORARY_INSTALLATION_DIR%\python.exe
 
@@ -11,8 +11,7 @@ rem see https://stackoverflow.com/questions/2817869/error-unable-to-find-vcvarsa
 rem For the sake of code safety, this should be the same framework used to build Python itself
 rem see http://p-nand-q.com/python/building-python-27-with-vs2010.html for more info
 set VS90COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools"
-echo %PYTHON_EXE%
-echo %MDANSE_TEMPORARY_INSTALLATION_DIR%
+
 "%GITHUB_WORKSPACE%\BuildServer\Windows\Build\python.exe" setup.py build build_api build_help install
 
 set STATUS=%ERRORLEVEL%
@@ -22,20 +21,20 @@ if %STATUS% neq 0 (
     exit %STATUS%
 )
 
-cd "%MDANSE_SOURCE_DIR%\BuildServer\Windows"
+cd "%GITHUB_WORKSPACE%\BuildServer\Windows"
 
 rem copy LICENSE
-copy %MDANSE_SOURCE_DIR%\\LICENSE %MDANSE_SOURCE_DIR%\\BuildServer\\Windows\\Resources\\nsis\\
+copy "%GITHUB_WORKSPACE%\\LICENSE" "%GITHUB_WORKSPACE%\\BuildServer\\Windows\\Resources\\nsis\\"
 
 rem copy CHANGELOG to CHANGELOG.txt (compulsory to be opened by nsis through an external text editor)
-copy %MDANSE_SOURCE_DIR%\CHANGELOG %MDANSE_SOURCE_DIR%\BuildServer\Windows\Resources\nsis\CHANGELOG.txt
+copy "%GITHUB_WORKSPACE%\CHANGELOG" "%GITHUB_WORKSPACE%\BuildServer\Windows\Resources\nsis\CHANGELOG.txt"
 
 rem Copy site.py 
-copy %MDANSE_SOURCE_DIR%\\BuildServer\\Windows\\Resources\\site.py %MDANSE_TEMPORARY_INSTALLATION_DIR%\\Lib\\
+copy "%GITHUB_WORKSPACE%\\BuildServer\\Windows\\Resources\\site.py" "%MDANSE_TEMPORARY_INSTALLATION_DIR%\\Lib\\"
 
 rem create the MDANSE installer
 echo "Creating nsis installer for target %MDANSE_TEMPORARY_INSTALLATION_DIR%..."
-makensis /V4 /ONSISlog.txt /DVERSION=%VERSION_NAME% /DARCH=%BUILD_TARGET% /DTARGET_DIR=%MDANSE_TEMPORARY_INSTALLATION_DIR% %MDANSE_SOURCE_DIR%\\BuildServer\\Windows\\Resources\\nsis\\MDANSE_installer.nsi
+makensis /V4 /ONSISlog.txt /DVERSION=%VERSION_NAME% /DARCH=%BUILD_TARGET% /DTARGET_DIR=%MDANSE_TEMPORARY_INSTALLATION_DIR% %GITHUB_WORKSPACE%\\BuildServer\\Windows\\Resources\\nsis\\MDANSE_installer.nsi
 
 set STATUS=%ERRORLEVEL%
 rem Exit now if something goes wrong
