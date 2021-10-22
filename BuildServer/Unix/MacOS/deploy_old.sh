9d00bc065e18804869cb47a68bd6611df24f3a88
Turanyi <rastislav.turanyi@stfc.ac.uk>
2021-10-22 21:32:46 +0100

    Place change_dylib_path.sh code into deploy_old.sh

--- BuildServer/Unix/MacOS/deploy_old.sh
+++ BuildServer/Unix/MacOS/deploy_old.sh
@@ -69,7 +69,20 @@ cp ${GITHUB_WORKSPACE}/BuildServer/Unix/MacOS/site.py ${MDANSE_APP_DIR}/Contents
 cp ${GITHUB_WORKSPACE}/BuildServer/Unix/MacOS/site.py ${MDANSE_APP_DIR}/Contents/Resources/lib/python2.7/.
 
 chmod 777 ${GITHUB_WORKSPACE}/BuildServer/Unix/MacOS/change_dylib_path.sh
-${GITHUB_WORKSPACE}/BuildServer/Unix/MacOS/change_dylib_path.sh
+
+# Change dylib paths
+files=`ls ${MDANSE_APP_DIR}/Contents/Frameworks/libwx*.dylib`
+
+libs="osx_cocoau_xrc osx_cocoau_webview osx_cocoau_html osx_cocoau_qa osx_cocoau_adv osx_cocoau_core baseu_xml baseu_net baseu"
+
+for f in $files
+do
+    chmod 777 $f
+    for l in $libs
+    do
+        install_name_tool -change /usr/local/lib/libwx_$l-3.0.dylib @executable_path/../Frameworks/libwx_$l-3.0.dylib $f
+    done
+done
 
 # Comment the 'add_system_python_extras' call that add some System path to the sys.path
 "${SED_I_COMMAND[@]}" "s/^add_system_python_extras()$/#add_system_python_extras()/" ${MDANSE_APP_DIR}/Contents/Resources/__boot__.py
