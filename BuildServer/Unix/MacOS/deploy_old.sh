53f0f55c962d2a28f7815199c9194c79768aadf3
Turanyi <rastislav.turanyi@stfc.ac.uk>
2021-10-28 15:43:49 +0100

    Create bin in resources first

--- BuildServer/Unix/MacOS/deploy_old.sh
+++ BuildServer/Unix/MacOS/deploy_old.sh
@@ -61,12 +61,8 @@ echo "${VERSION_NAME}" | sudo tee "${MDANSE_APP_DIR}/Contents/Resources/version"
 ### In our case we also want the user to be able to start directly python without launching the bundle executable (e.g. to run scripts in command line) which is the reason
 ### why we have to modify the python executable appropriately with the following commands
 echo -e "${BLUE}""Copying python""${NORMAL}"
-sudo cp /System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python ${MDANSE_APP_DIR}/Contents/Resources/bin
-echo "Rename the Python file"
-cd "${MDANSE_APP_DIR}/Contents/Resources/bin"
-echo "asdgaaaa"
-sudo mv Python python
-cd ${GITHUB_WORKSPACE}
+mkdir -p ${MDANSE_APP_DIR}/Contents/Resources/bin
+sudo cp /System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python ${MDANSE_APP_DIR}/Contents/Resources/bin/python
 
 echo "Copy lib"
 sudo cp -r $HOME/Contents/Resources/lib ${MDANSE_APP_DIR}/Contents/Resources
