06d98c181284e9bff2a8a908cbb12a8c9af569b9
Turanyi <rastislav.turanyi@stfc.ac.uk>
2021-10-29 10:47:54 +0100

    Add investigation and use * instead of @

--- BuildServer/Unix/MacOS/change_dylib_path.sh
+++ BuildServer/Unix/MacOS/change_dylib_path.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 echo $GITHUB_WORKSPACE
 echo "-------------------------------------"
-files=("${MDANSE_APP_DIR}/Contents/Frameworks/libwx*.dylib")
-
+ls /Users/runner/work/MDANSE/MDANSE/temp/dist/MDANSE.app/Contents/Frameworks/libwx*.dylib
+echo "-------------------------------------"
+files=("$GITHUB_WORKSPACE/temp/dist/MDANSE.app/Contents/Frameworks/libwx*.dylib")
 echo "${files[@]}"
+echo "-------------------------------------"
+echo "${files[*]}"
+echo "-------------------------------------"
+
 libs="osx_cocoau_xrc osx_cocoau_webview osx_cocoau_html osx_cocoau_qa osx_cocoau_adv osx_cocoau_core baseu_xml baseu_net baseu"
 
-for f in "${files[@]}"
+for f in ${files[*]}
 do
+    echo $f
     sudo chmod 777 $f
     for l in $libs
     do
+        echo $l
         sudo install_name_tool -change /usr/local/lib/libwx_$l-3.0.dylib @executable_path/../Frameworks/libwx_$l-3.0.dylib $f
     done
 done
