0c7e321dc3a4fcc8d7dbb1c846f6fdc19ae8f3b3
Turanyi <rastislav.turanyi@stfc.ac.uk>
2021-11-02 15:00:56 +0000

    Actually correct typos in dylib names

--- BuildServer/Unix/MacOS/change_dylib_path.sh
+++ BuildServer/Unix/MacOS/change_dylib_path.sh
@@ -4,7 +4,7 @@ cd "$GITHUB_WORKSPACE/temp/dist/MDANSE.app/Contents/Frameworks/"
 files=(libwx*.dylib)
 
 libs="osx_cocoau_xrc osx_cocoau_webview osx_cocoau_html osx_cocoau_qa osx_cocoau_adv osx_cocoau_core baseu_xml baseu_net baseu"
-
+echo "Changing links inside Frameworks"
 for f in ${files[*]}
 do
     sudo chmod 777 $f
@@ -15,14 +15,15 @@ do
     done
 done
 
+echo "Changing links inside lib-dynload"
 cd ${MDANSE_APP_DIR}/Contents/Resources/lib/python2.7/lib-dynload/wx
 files=(libwx*.dylib)
-
 for f in ${files[*]}
 do
   sudo install_name_tool -change @rpath/libz.1.dylib @executable_path/../Frameworks/libz.1.dylib $f
-  sudo install_name_tool -change @rpath/liconv.2.dylib @executable_path/../Frameworks/liconv.2.dylib $f
-  sudo install_name_tool -change @rpath/lic++.1.dylib @executable_path/../Frameworks/lic++.1.dylib $f
+  sudo install_name_tool -change @rpath/libiconv.2.dylib @executable_path/../Frameworks/libiconv.2.dylib $f
+  sudo install_name_tool -change @rpath/libc++.1.dylib @executable_path/../Frameworks/libc++.1.dylib $f
 done
+
 
 cd $GITHUB_WORKSPACE
