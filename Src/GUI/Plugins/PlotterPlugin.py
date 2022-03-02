087e1d6fd8f99abf7b0a1257a5587fd1e3196334
Turanyi <rastislav.turanyi@stfc.ac.uk>
2022-03-02 15:44:59 +0000

    Remove certain variables from plotting

--- Src/GUI/Plugins/PlotterPlugin.py
+++ Src/GUI/Plugins/PlotterPlugin.py
@@ -222,7 +222,8 @@ class DataPanel(wx.Panel):
         
     def show_data(self):
         self.datalist.DeleteAllItems()
-        for i, var in enumerate(sorted(self.dataproxy.keys())):
+        variables = [key for key in self.dataproxy.keys() if key not in ['description', 'step', 'time']]
+        for i, var in enumerate(sorted(variables)):
             self.datalist.InsertStringItem(i, var)
             #self.datalist.SetStringItem(i, 1,self.dataproxy[var]['units'])
             axis = ','.join(self.dataproxy[var]['axis'])
