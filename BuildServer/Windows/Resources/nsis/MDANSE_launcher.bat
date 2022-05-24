6ba64de7655ea326fd8e57529c9bf2ea21658ab8
Turanyi <rastislav.turanyi@stfc.ac.uk>
2022-05-24 16:54:51 +0100

    Correct path in .bat and try setting path

--- BuildServer/Windows/Resources/nsis/MDANSE_launcher.bat
+++ BuildServer/Windows/Resources/nsis/MDANSE_launcher.bat
@@ -5,8 +5,8 @@
 set PYTHONHOME=
 set PYTHONPATH=
 
-set dirname="%~dp0"
+set dirname=%~dp0
 
-set PATH=%dirname%\DLLs;%dirname%\Include;%dirname%\libs;%PATH%
+set PATH=%dirname%DLLs;%dirname%Include;%dirname%libs;%PATH%
 
 start /B /D %dirname% pythonw.exe Scripts\mdanse_gui
