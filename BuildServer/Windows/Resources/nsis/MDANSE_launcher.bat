97cd00b0ee3d974c275dcea085cb64bb0485683c
Turanyi <rastislav.turanyi@stfc.ac.uk>
2022-05-25 09:33:34 +0100

    Copy all conda dlls etc

--- BuildServer/Windows/Resources/nsis/MDANSE_launcher.bat
+++ BuildServer/Windows/Resources/nsis/MDANSE_launcher.bat
@@ -9,4 +9,4 @@ set dirname=%~dp0
 
 set PATH=%dirname%DLLs;%dirname%Include;%dirname%libs;%PATH%
 
-start /B /D %dirname% pythonw.exe Scripts\mdanse_gui
+start /B /D "%dirname%" pythonw.exe Scripts\mdanse_gui
