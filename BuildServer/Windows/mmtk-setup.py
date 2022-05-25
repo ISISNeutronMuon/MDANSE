5499fe0cfebc68c2eeec9415f412faedfdd49959
Turanyi <rastislav.turanyi@stfc.ac.uk>
2022-05-25 15:22:18 +0100

    Copy netcdf.h directly to include on win CD

--- BuildServer/Windows/mmtk-setup.py
+++ BuildServer/Windows/mmtk-setup.py
@@ -47,7 +47,7 @@ netcdf_h_search_paths = [os.environ.get('NETCDF_HEADER_FILE_PATH', '/tmp/'),
                          os.path.join(sys.prefix,'include',python_version,'Scientific'),
                          os.path.join(sys.prefix,'local','include',python_version,'Scientific'),
                          os.path.join(site.USER_BASE,'include',python_version,'Scientific'),
-                         os.path.join(os.environ.get('CONDA', r'C:\Miniconda'), 'envs', 'mdanse', 'Include', 'Scientific')]
+                         os.path.join(os.environ.get('CONDA', r'C:\Miniconda'), 'envs', 'mdanse', 'Include')]
 netcdf_h = None
 for path in netcdf_h_search_paths:
     test_path = os.path.join(path,'netcdf.h')
