2a2ce0e6e0196e049fa1f22f1bfd2020d3673a9b
eric pellegrini <pellegrini@ill.fr>
2022-06-10 15:32:21 +0200

    Fixed git regression in JobStatus

--- Src/Framework/Jobs/JobStatus.py
+++ Src/Framework/Jobs/JobStatus.py
@@ -13,12 +13,18 @@
 #
 # **************************************************************************
 
+import collections
+import pickle
 import datetime
-
-import zmq
+import os
+import threading
+import time
 
 from MDANSE import PLATFORM
 from MDANSE.Framework.Status import Status
+
+class JobState(collections.OrderedDict):
+    pass
 
 class JobStatus(Status):
          
@@ -26,7 +32,7 @@ class JobStatus(Status):
 
         Status.__init__(self)
                                  
-        self._state = {}
+        self._state = JobState()
         self._state['pid'] = PLATFORM.pid() 
         self._state['type'] = job.__class__.__name__
         self._state['start'] = datetime.datetime.strftime(datetime.datetime.today(),"%d-%m-%Y %H:%M:%S")
@@ -35,31 +41,42 @@ class JobStatus(Status):
         self._state['current_step'] = 0 
         self._state['n_steps'] = 0 
         self._state['progress'] = 0 
-        self._state['state'] = "setup"
+        self._state['state'] = "running"
         self._state['name'] = job.name
         self._state['traceback'] = ''
+        self._state['temporary_file'] = os.path.join(PLATFORM.temporary_files_directory(), self._state['name'])
         self._state['info'] = ''
-
-        self._context = zmq.Context()
-
-        self._server = self._context.socket(zmq.PUB)
-        self._server.connect("tcp://127.0.0.1:6789")
-                                        
+        
+        self.save_status()
+                    
+    def finish_status(self):
+        # Launch a thread that will wait before erasing the file
+        t= threading.Thread(target = self.cleanup)
+        t.start()
+                    
     @property
     def state(self):
         
         return self._state    
 
-    def send_status(self):
-
-        self._server.send_pyobj(self._state,protocol=2)
-      
+    def cleanup(self):
+        # Wait before cleaning
+        time.sleep(PLATFORM.jobs_launch_delay())
+        try:
+            os.unlink(self._state['temporary_file'])
+        except:
+            pass
+                 
     def start_status(self):
                         
         self._state["n_steps"] = self.nSteps
-        self._state['state'] = "running"
 
-        self.send_status()
+        self.save_status()
+                                
+    def save_status(self):
+
+        with open(self._state['temporary_file'], 'wb') as f:
+            pickle.dump(self._state, f, protocol=2)
 
     def stop_status(self):
         pass
@@ -75,6 +92,5 @@ class JobStatus(Status):
             self._eta = "N/A"
             self._state['progress'] = 0
         
-        self.send_status()
-        
+        self.save_status()
         
