cf591a2a3d3c230a504107a2ba58894a54e95591
Turanyi <rastislav.turanyi@stfc.ac.uk>
2022-06-14 11:12:34 +0100

    Keep the main window size constant

--- Src/GUI/MainFrame.py
+++ Src/GUI/MainFrame.py
@@ -418,17 +418,8 @@ Authors:
         self.toggle_window(pane)    
 
     def on_toggle_toolbar(self, event=None):
-
-        self._prevSize = self.GetSize()
-
-        if self._toolbar.IsShown():
-            sizeH = self._prevSize[1] - self._toolbar.GetSize()[1]
-            self._toolbar.Hide()
-        else:
-            sizeH = self._prevSize[1] + self._toolbar.GetSize()[1]
-            self._toolbar.Show()
-            
-        self.SetSize((-1,sizeH))
+        self.toggle_window(self._toolbar)
+        self.Layout()
             
     @property
     def panels(self):
@@ -442,7 +433,8 @@ Authors:
             window.Show()
 
         self._mgr.Update()
-        
+
+
 if __name__ == "__main__":
     
     from MDANSE.GUI.Apps import MainApplication
