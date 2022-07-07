ef4f66b910e8502aede9ae58af75c1276534a30d
Turanyi <rastislav.turanyi@stfc.ac.uk>
2022-07-07 11:50:50 +0100

    Set paths in MDANSE_command_shell.bat

--- BuildServer/Windows/Resources/nsis/MDANSE_command_shell.bat
+++ BuildServer/Windows/Resources/nsis/MDANSE_command_shell.bat
@@ -7,7 +7,8 @@ COLOR B
 TITLE MDANSE command shell
 
 :: append python path to the PATH
-SET PATH="%~dp0";%PATH%
+set dirname=%~dp0
+set PATH=%dirname%\Library;%dirname%\Library\bin;%dirname%\Library\include;%dirname%\Library\lib;%dirname%DLLs;%dirname%Include;%dirname%libs;%PATH%
 
 :: get rid of any third-party program that may alter the PYTHONHOME and PYTHONPATH
 SET PYTHONHOME=
