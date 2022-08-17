029cdb958b73c2f00240474c85fad5c1c53324ed
Turanyi <rastislav.turanyi@stfc.ac.uk>
2022-08-17 09:08:54 +0100

    Add missing tests to suite function

--- Tests/UnitTests/TestDatabases.py
+++ Tests/UnitTests/TestDatabases.py
@@ -13,37 +13,11 @@
 #
 # **************************************************************************
 
-# MDANSE : Molecular Dynamics Analysis for Neutron Scattering Experiments
-# ------------------------------------------------------------------------------------------
-# Copyright (C)
-# 2015- Eric C. Pellegrini Institut Laue-Langevin
-# BP 156
-# 6, rue Jules Horowitz
-# 38042 Grenoble Cedex 9
-# France
-# pellegrini[at]ill.fr
-# goret[at]ill.fr
-# aoun[at]ill.fr
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-''' 
+"""
 Created on May 29, 2015
 
 @author: Eric C. Pellegrini
-'''
+"""
 
 import json
 import os
@@ -506,6 +480,9 @@ def suite():
     loader = unittest.TestLoader()
     s = unittest.TestSuite()
     s.addTest(loader.loadTestsFromTestCase(TestAtomsDatabase))
+    s.addTest(loader.loadTestsFromTestCase(TestMoleculesDatabase))
+    s.addTest(loader.loadTestsFromTestCase(TestNucleotidesDatabase))
+    s.addTest(loader.loadTestsFromTestCase(TestResiduesDatabase))
     return s
 
 
