0ac6c9d6e0c747a8a781be5a6334e8bc99c43a34
Maciej Bartkowiak <108934199+MBartkowiakSTFC@users.noreply.github.com>
2022-09-23 16:43:48 +0100

    Add the BackEnd class

---
+++ Src/PyQtGUI/BackEnd.py
@@ -0,0 +1,27 @@
+# **************************************************************************
+#
+# MDANSE: Molecular Dynamics Analysis for Neutron Scattering Experiments
+#
+# @file      Src/PyQtGUI/BackEnd.py
+# @brief     Everything that is not GUI is accessed through the BackEnd
+#
+# @homepage  https://mdanse.org
+# @license   GNU General Public License v3 or higher (see LICENSE)
+# @copyright Institut Laue Langevin 2013-now
+# @copyright ISIS Neutron and Muon Source, STFC, UKRI 2021-now
+# @authors   Research Software Group at ISIS (see AUTHORS)
+#
+# **************************************************************************
+
+from PyQt6.QtCore import pyqtSlot, QObject, QThread, QMutex, pyqtSignal
+
+class BackEnd(QObject):
+    """The main window of the MDANSE GUI,
+    inherits QMainWindow.
+
+    Args:
+        QMainWindow - the base class.
+    """
+    def __init__(self, title = "MDANSE"):
+        self.setWindowTitle(title)
+        
