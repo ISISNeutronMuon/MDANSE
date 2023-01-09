e78fa35964770d2e26c6c026d518f4635f30bab8
Maciej Bartkowiak <108934199+MBartkowiakSTFC@users.noreply.github.com>
2023-01-09 16:48:07 +0000

    Connect the save function to the ElementDatabaseEditor

--- Src/PyQtGUI/ElementsDatabaseEditor.py
+++ Src/PyQtGUI/ElementsDatabaseEditor.py
@@ -24,6 +24,8 @@ class ElementModel(QStandardItemModel):
         self.database = element_database
         self.parseDatabase()
 
+        self.itemChanged.connect(self.write_to_database)
+
     def parseDatabase(self):
 
         all_column_names = self.database.properties
@@ -54,6 +56,25 @@ class ElementModel(QStandardItemModel):
         self.setHorizontalHeaderLabels(all_column_names)
         self.setVerticalHeaderLabels(all_row_names)
 
+        self.all_column_names = all_column_names
+        self.all_row_names = all_row_names
+    
+    @Slot('QStandardItem*')
+    def write_to_database(self, item: 'QStandardItem'):
+        data = item.data()
+        text = item.text()
+        row = item.row()
+        column = item.column()
+        print(f"data:{data}, text:{text},row:{row},column:{column}")
+        print(f"column name={self.all_column_names[column]}, row name={self.all_row_names[row]}")
+        self.database.set_value(self.all_row_names[row], self.all_column_names[column], text)
+        self.save_changes()
+
+    @Slot()
+    def save_changes(self):
+
+        self.database.save()
+
 
 class ElementsDatabaseEditor(QDialog):
 
@@ -75,6 +96,7 @@ class ElementsDatabaseEditor(QDialog):
 
         self.proxy_model.setSourceModel(self.data_model)
         self.viewer.setModel(self.proxy_model)
+
 
 if __name__ == '__main__':
     import sys
