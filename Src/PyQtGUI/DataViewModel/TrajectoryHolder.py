59e87893260e48c57a93816c2df363aa21374c3f
Maciej Bartkowiak <108934199+MBartkowiakSTFC@users.noreply.github.com>
2023-01-20 14:24:07 +0000

    Add nodes to the TrajectoryHolder

--- Src/PyQtGUI/DataViewModel/TrajectoryHolder.py
+++ Src/PyQtGUI/DataViewModel/TrajectoryHolder.py
@@ -13,6 +13,8 @@
 #
 # **************************************************************************
 
+import hashlib
+
 from qtpy.QtGui import QStandardItemModel, QStandardItem
 from qtpy.QtCore import QObject, Slot
 
@@ -35,3 +37,65 @@ class TrajectoryHolder(QStandardItemModel):
         traj = TrajectoryItem(new_entry.basename, trajectory = new_entry)
         self.appendRow([traj])
 
+
+class FileObject():
+
+    def __init__(self, hash_function = hashlib.sha256):
+
+        self.filename = ""
+        self.absolute_path = ""
+        self.relative_path = '.'
+        self.hash_function = hash_function
+
+
+class DataTreeItem(QStandardItem):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            self.mdanse_tag = kwargs['mdanse_tag']
+        except KeyError:
+            self.mdanse_tag = 'unknown'
+        
+        self.original_input = []
+        self.processing_used = ""
+        self.processing_parameters = {}
+
+    def showHistory(self):
+        history = ";".join(self.ancestors())
+        return history
+    
+    def ancestors(self):
+        parent = self.parent()
+        if parent is None:
+            return [self.mdanse_tag]
+        else:
+            return parent.ancestors() + [self.mdanse_tag]
+
+
+class TrajectoryItem(DataTreeItem):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.original_input = []
+        self.processing_used = ""
+        self.processing_parameters = {}
+
+
+class AnalysisItem(DataTreeItem):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+
+class DataTreeModel(QStandardItemModel):
+
+    def __init__(self, parent: QObject = None):
+        super().__init__(parent=parent)
+
+    @Slot(object)
+    def addItem(self, new_entry: InputFileData):
+        traj = TrajectoryItem(new_entry.basename, trajectory = new_entry)
+        self.appendRow([traj])
