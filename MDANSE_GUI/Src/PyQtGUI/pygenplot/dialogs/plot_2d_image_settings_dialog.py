cd6ef9e21234fc42069b556b7b91012584d15951
Maciej Bartkowiak <maciej.bartkowiak@stfc.ac.uk>
2023-09-07 13:24:32 +0100

    Add copyright header to Eric's code

--- MDANSE_GUI/Src/PyQtGUI/pygenplot/dialogs/plot_2d_image_settings_dialog.py
+++ MDANSE_GUI/Src/PyQtGUI/pygenplot/dialogs/plot_2d_image_settings_dialog.py
@@ -1,3 +1,17 @@
+# **************************************************************************
+#
+# MDANSE: Molecular Dynamics Analysis for Neutron Scattering Experiments
+#
+# @file      Src/PyQtGUI/pygenplot/__init__.py
+# @brief     root file of pygenplot
+#
+# @homepage  https://mdanse.org
+# @license   GNU General Public License v3 or higher (see LICENSE)
+# @copyright Institut Laue Langevin 2023-now
+# @authors   Eric Pellegrini
+#
+# **************************************************************************
+
 from PyQt6 import QtWidgets
 
 
--- MDANSE_GUI/Src/PyQtGUI/pygenplot/widgets/range_slider.py
+++ MDANSE_GUI/Src/PyQtGUI/pygenplot/widgets/range_slider.py
@@ -1,3 +1,17 @@
+# **************************************************************************
+#
+# MDANSE: Molecular Dynamics Analysis for Neutron Scattering Experiments
+#
+# @file      Src/PyQtGUI/pygenplot/__init__.py
+# @brief     root file of pygenplot
+#
+# @homepage  https://mdanse.org
+# @license   GNU General Public License v3 or higher (see LICENSE)
+# @copyright Institut Laue Langevin 2023-now
+# @authors   Eric Pellegrini
+#
+# **************************************************************************
+
 # Code taken from https://stackoverflow.com/questions/67028200/pyqt5-qslider-two-positions
 
 from qtpy import QtCore, QtGui, QtWidgets
