287b2598b233c710eb6dc585cee15420f32cb8d0
Ashley Meigh <ashleymeigh0gmail.com>
2023-11-14 10:38:15 +0000

    chnages made

--- MDANSE/Src/Framework/Formats/NetCDF_HDF_convert.py
+++ MDANSE/Src/Framework/Formats/NetCDF_HDF_convert.py
@@ -21,52 +21,82 @@ Proper exception handling and file management are implemented.
 For more information about the MDANSE project, visit: https://www.isis.stfc.ac.uk/Pages/MDANSEproject.aspx
 
 """
-import os
+import abc
 import h5py
 from netCDF4 import Dataset
 from MDANSE.Chemistry.ChemicalEntity import Atom, ChemicalSystem
  
-def convert_trajectory_to_hdf5(trajectory_filename, h5_filename):
-    try:
-        chemical_system = None
-        with Dataset(trajectory_filename, 'r') as trajectory_file:
-            chemical_entity_data = None
-            for varname in trajectory_file.variables.keys():
-                var = trajectory_file.variables[varname]
-                if 'chemical_entity' in varname:
-                    chemical_entity_data = var[:]
+class TrajectoryConverter(abc.ABC):
  
-            with h5py.File(h5_filename, 'w') as h5_file:
-                for dimname in trajectory_file.dimensions.keys():
-                    dim = trajectory_file.dimensions[dimname]
-                    h5_file.create_dataset(dimname, data=dim.size)
+    @abc.abstractmethod
+    def serialize(self, h5_file):
+        """ Abstract method to serialize data into HDF5 format. """
+        pass
  
-                for varname in trajectory_file.variables.keys():
-                    var = trajectory_file.variables[varname]
-                    if varname != 'chemical_entity':
-                        h5_file.create_dataset(varname, data=var[:])
+class HDF5TrajectoryConverter(TrajectoryConverter):
+    def __init__(self, trajectory_filename):
+        self.trajectory_filename = trajectory_filename
+        self.chemical_system = None
  
-                if chemical_entity_data is not None:
-                    atoms = []
-                    for data in chemical_entity_data:
-                        atom = Atom(*data)
-                        atoms.append(atom)
-                    chemical_system = ChemicalSystem(atoms) 
+    def serialize(self, h5_file, trajectory_file, chemical_entity_data):
+        if chemical_entity_data is not None:
+            atoms = []
+            for data in chemical_entity_data:
+                atom = Atom(*data)
+                atoms.append(atom)
+            self.chemical_system = ChemicalSystem(atoms)
+            chemical_group = h5_file.create_group("ChemicalSystem")
+            for i, atom in enumerate(self.chemical_system.get_atoms()):
+                atom_group = chemical_group.create_group(f"Atom_{i}")
+                atom_group.create_dataset("element", data=atom.element)
+                atom_group.create_dataset("mass", data=atom.mass)
+                atom_group.create_dataset("position", data=atom.position)
  
-                    chemical_group = h5_file.create_group("ChemicalSystem")
-                    for i, atom in enumerate(chemical_system.get_atoms()):
-                        atom_group = chemical_group.create_group(f"Atom_{i}")
-                        atom_group.create_dataset("element", data=atom.element)
-                        atom_group.create_dataset("mass", data=atom.mass)
-                        atom_group.create_dataset("position", data=atom.position)
+    def write_configuration(self, h5_file, trajectory_file):
+        configuration_grp = h5_file.create_group("/configuration")
+        for varname, var in trajectory_file.variables.items():
+            if varname not in ['time', 'unit_cell', 'chemical_entity']:  
+                data = var[:]
+                dtype = data.dtype 
+                dset = configuration_grp.create_dataset(
+                    varname,
+                    data=data,
+                    dtype=dtype,
+                    compression='gzip')
+                if hasattr(var, 'units'):
+                    dset.attrs["units"] = var.units
+        if 'unit_cell' in trajectory_file.variables:
+            unit_cell_grp = h5_file.create_group("/unit_cell")
+            unit_cell = trajectory_file.variables['unit_cell'][:]
+            dtype = unit_cell.dtype  
+            unit_cell_dset = unit_cell_grp.create_dataset(
+                "data",
+                data=unit_cell,
+                dtype=dtype)
+            if hasattr(trajectory_file.variables['unit_cell'], 'units'):
+                unit_cell_dset.attrs["units"] = trajectory_file.variables['unit_cell'].units
+        if 'time' in trajectory_file.variables:
+            time_grp = h5_file.create_group("/time")
+            time = trajectory_file.variables['time'][:]
+            dtype = time.dtype  
+            time_dset = time_grp.create_dataset(
+                "data",
+                data=time,
+                dtype=dtype)
+            if hasattr(trajectory_file.variables['time'], 'units'):
+                time_dset.attrs["units"] = trajectory_file.variables['time'].units
  
-                if chemical_system:
-                    print(f"Converted {trajectory_filename} to {h5_filename} and stored chemical system information.")
-                else:
-                    print(f"Converted {trajectory_filename} to {h5_filename}")
+    def convert_trajectory_to_hdf5(self, h5_filename):
+        try:
+            with Dataset(self.trajectory_filename, 'r') as trajectory_file:
+                chemical_entity_data = None
+                if 'chemical_entity' in trajectory_file.variables:
+                    chemical_entity_data = trajectory_file.variables['chemical_entity'][:]
  
-    except Exception as e:
-        print(f"Error converting {trajectory_filename} to HDF5: {e}")
+                with h5py.File(h5_filename, 'w') as h5_file:
+                    self.serialize(h5_file, trajectory_file, chemical_entity_data)
+                    self.write_configuration(h5_file, trajectory_file)
+                    print(f"Converted {self.trajectory_filename} to {h5_filename}.")
+        except Exception as e:
+            print(f"Error converting {self.trajectory_filename} to HDF5: {e}")
  
-# Example usage with the provided file paths
-convert_trajectory_to_hdf5('C:/Users/ttr36747/Documents/MDANSE/MDANSE/Data/Trajectories/traj/current_traj.nc', 'C:/Users/ttr36747/Documents/MDANSE/MDANSE/Data/Trajectories/traj/new_trajectory.h5')
