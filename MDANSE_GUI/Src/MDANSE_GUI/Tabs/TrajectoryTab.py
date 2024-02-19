9ed7b75fa5367faaac930fa8ca1b1cf68b30a1d7
Chi Cheng <chicheng85@gmail.com>
2024-02-19 14:15:59 +0000

    updated label text to add previous tabs information

--- MDANSE_GUI/Src/MDANSE_GUI/Tabs/TrajectoryTab.py
+++ MDANSE_GUI/Src/MDANSE_GUI/Tabs/TrajectoryTab.py
@@ -29,7 +29,16 @@ from MDANSE_GUI.Tabs.Visualisers.TrajectoryInfo import TrajectoryInfo
 from MDANSE_GUI.Tabs.Visualisers.View3D import View3D
 
 
+label_text = """Here you can load the .mdt files.
+They are MD trajectories in HDF5 format created by one of the MDANSE converters.
+You can select trajectories. They will be visualised in the 3D view window.
+The animation of the MD trajectory will allow you to verify if the contents 
+of the trajectory are what you expected.
+"""
+
+
 class TrajectoryTab(GeneralTab):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._core.add_button("Load an .MDT Trajectory", self.load_trajectory)
@@ -61,10 +70,7 @@ class TrajectoryTab(GeneralTab):
             view=TrajectoryView(),
             visualiser=View3D(),
             layout=partial(TriplePanel, left_panel=TrajectoryInfo()),
-            label_text="""Here you can load the .mdt files.
-They are MD trajectories in HDF5 format,
-created by one of the MDANSE converters.
-""",
+            label_text=label_text,
         )
         return the_tab
 
@@ -88,10 +94,7 @@ created by one of the MDANSE converters.
             view=TrajectoryView(),
             visualiser=View3D(),
             layout=partial(TriplePanel, left_panel=TrajectoryInfo()),
-            label_text="""Here you can load the .mdt files.
-They are MD trajectories in HDF5 format,
-created by one of the MDANSE converters.
-""",
+            label_text=label_text,
         )
         return the_tab
 
