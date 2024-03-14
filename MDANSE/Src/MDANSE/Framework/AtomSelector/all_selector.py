f0918e7a7151680e07a4105db9c52fc694bbe09e
Chi Cheng <chicheng85@gmail.com>
2024-03-14 12:21:34 +0000

    changed all selection so that it does not include dummy atoms. added selection for dummy atoms

--- MDANSE/Src/MDANSE/Framework/AtomSelector/all_selector.py
+++ MDANSE/Src/MDANSE/Framework/AtomSelector/all_selector.py
@@ -5,7 +5,8 @@ from MDANSE.Chemistry.ChemicalEntity import ChemicalSystem
 def select_all(
     system: ChemicalSystem, check_exists: bool = False
 ) -> Union[set[int], bool]:
-    """Selects all atoms in the chemical system.
+    """Selects all atoms in the chemical system except for the dummy
+    atoms.
 
     Parameters
     ----------
@@ -17,9 +18,10 @@ def select_all(
     Returns
     -------
     Union[set[int], bool]
-        All atom indices or a bool if checking match.
+        All atom indices except for dummy atoms or a bool if checking
+        match.
     """
     if check_exists:
         return True
     else:
-        return set([at.index for at in system.atom_list])
+        return set([at.index for at in system.atom_list if at.element != "dummy"])
