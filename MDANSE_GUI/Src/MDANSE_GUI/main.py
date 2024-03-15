b70e685c4bddabc17b6440179c339f4647d085c3
Chi Cheng <chicheng85@gmail.com>
2024-03-15 15:41:54 +0000

    adjusted so the way the splash screen load and closes

--- MDANSE_GUI/Src/MDANSE_GUI/main.py
+++ MDANSE_GUI/Src/MDANSE_GUI/main.py
@@ -17,7 +17,7 @@ import os
 import time
 
 from qtpy.QtWidgets import QApplication, QSplashScreen
-from qtpy.QtCore import QSettings
+from qtpy.QtCore import QSettings, Qt, QTimer
 from qtpy.QtGui import QPixmap
 
 from MDANSE_GUI.TabbedWindow import TabbedWindow
@@ -32,15 +32,15 @@ def startGUI(some_args):
 
     path = os.path.dirname(os.path.abspath(__file__))
     splash_img = QPixmap(os.path.join(path, "splash.png"))
-    splash = QSplashScreen(splash_img)
+    splash = QSplashScreen(splash_img, Qt.WindowStaysOnTopHint)
     splash.show()
-    # show the splash screen for at least 1 sec
-    time.sleep(1)
-
+    t0 = time.time()
     root = TabbedWindow(parent=None, title="MDANSE for Python 3", settings=settings)
     root.show()
-
-    splash.finish(root)
+    t1 = time.time()
+    # only allow the splash screen to stay up for maximum of 2s if the
+    # GUI loads fast but closes immediately if GUI was slow to load
+    QTimer.singleShot(max([0, round(2000 - 1000 * (t1 - t0))]), lambda: splash.finish(root))
 
     app.exec()  # once this is done, the GUI has its event loop running.
     # no more Python scripting now, we are in the event loop.
