446fcd3185ecd98f9be7cb7322941f0970ac8856
Maciej Bartkowiak <maciej.bartkowiak@stfc.ac.uk>
2024-07-05 15:24:09 +0100

    Correct the dependencies of MDANSE_GUI

--- MDANSE_GUI/Src/MDANSE_GUI/UserSettingsEditor.py
+++ MDANSE_GUI/Src/MDANSE_GUI/UserSettingsEditor.py
@@ -14,7 +14,7 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-from PyQt6.QtWidgets import QWidget
+
 from qtpy.QtWidgets import (
     QDialog,
     QPushButton,
