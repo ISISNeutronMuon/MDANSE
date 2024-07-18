8726b5341dd132b4187ff016768356eba69693fe
Chi Cheng <chicheng85@gmail.com>
2024-07-18 11:32:19 +0100

    corrected file headers

--- MDANSE/Src/MDANSE/Framework/Configurators/MoleculeSelectionConfigurator.py
+++ MDANSE/Src/MDANSE/Framework/Configurators/MoleculeSelectionConfigurator.py
@@ -1,5 +1,4 @@
-# **************************************************************************
-#    This file is part of MDANSE_GUI.
+#    This file is part of MDANSE.
 #
 #    MDANSE_GUI is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
