2ac860172708b744acb063d797d421f1110334d4
RobBuchananCompPhys <robertbb269@hotmail.com>
2025-02-10 11:24:18 +0000

    remove unused import

--- MDANSE/Src/MDANSE/Mathematics/Signal.py
+++ MDANSE/Src/MDANSE/Mathematics/Signal.py
@@ -13,7 +13,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-import numpy
 import numpy as np
 from enum import Enum
 from collections import namedtuple
