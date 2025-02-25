4957efb83e634fbcf4be20a2c30215255fe621b8
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2025-02-25 17:11:16 +0000

    Convert test_editor to compare_mdt

--- MDANSE/Tests/CommonFunctions/compare_mdt.py
+++ MDANSE/Tests/CommonFunctions/compare_mdt.py
@@ -3,7 +3,8 @@ from pathlib import Path
 import h5py
 from typing import Sequence
 
-def compare_mdt(result_path: Path, benchmark_path: Path, comparison_keys: Sequence[str]) -> None:
+def compare_mdt(result_path: Path, benchmark_path: Path,
+                comparison_keys: Sequence[str]) -> None:
     """
     Compare two h5py files by the keys given in comparison_keys.
 
@@ -19,4 +20,8 @@ def compare_mdt(result_path: Path, benchmark_path: Path, comparison_keys: Sequen
 
     with h5py.File(result_path) as result, h5py.File(benchmark_path) as benchmark:
         for key in comparison_keys:
-            np.testing.assert_array_almost_equal(result[key], benchmark[key])
+            if isinstance(key, (tuple, list)):
+                key, subset = key
+            else:
+                subset = slice(None)
+            np.testing.assert_array_almost_equal(result[key], benchmark[key][subset])
