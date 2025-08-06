171834d41b7f1fb89eff3b64e20c0b5edd6b5591
Maciej Bartkowiak <maciej.bartkowiak@stfc.ac.uk>
2025-08-06 16:02:51 +0100

    Create subparsers for mdanse.py options

--- MDANSE/Src/MDANSE/Scripts/mdanse.py
+++ MDANSE/Src/MDANSE/Scripts/mdanse.py
@@ -15,7 +15,7 @@
 #
 from __future__ import annotations
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
 import MDANSE
@@ -30,12 +30,14 @@ class CommandLineParserError(Error):
     pass
 
 
-def show_element_info(element: str | None):
+def show_element_info(args: Namespace):
+    element = args.element_name
     if element:
         print(ATOMS_DATABASE.info(element))  # noqa: T201
 
 
-def show_trajectory_contents(trajectory_path: str | Path | None):
+def show_trajectory_contents(args: Namespace):
+    trajectory_path = args.file_name
     if not trajectory_path:
         return
     trajName = Path.cwd() / trajectory_path
@@ -82,32 +84,99 @@ def save_job(
     job.save(script_name)
 
 
-def produce_output(options: dict[str, str], args: list[str]):
-    print(options)
-    show_element_info(options.element)
-    show_trajectory_contents(options.trajectory)
-    show_jobs(options.job)
-
-
-def main():
+def build_parsers() -> ArgumentParser:
     parser = ArgumentParser(
-        prog="MDANSE CLI",
+        prog="mdanse",
         description="This is the command line interface of MDANSE "
         "(Molecular Dynamics Analysis for Neutron Scattering Experiments).",
         epilog="Please report any problems with MDANSE as issues on https://github.com/ISISNeutronMuon/MDANSE",
     )
-    parser.add_argument("-t", "--traj")
     subparsers = parser.add_subparsers(title="MDANSE CLI Commands")
+    # Set up element options.
     element = subparsers.add_parser(
-        "element", help="View chemical element information."
+        "element",
+        help="View chemical element information.",
+        description="MDANSE stores chemical element properties in a central database. "
+        "When you convert trajectories, the properties of the relevant atoms are written into the trajectory file. "
+        "This command can be used to list, find and view atom properties in specific files.",
     )
+    element.set_defaults(func=show_element_info)
+    element.add_argument(
+        "element_name",
+        help="Symbol of the chemical element or isotope, e.g. Au, Li7, etc.",
+    )
+    element.add_argument(
+        "-t", "--traj", help="Use this trajectory file as atom database."
+    )
+    element.add_argument(
+        "-s", "--search", help="Find chemical elements with matching names."
+    )
+    element.add_argument(
+        "-l", "--list", help="List all the chemical elements in the database."
+    )
+    # Set up trajectory options.
     trajectory = subparsers.add_parser(
-        "traj", help="View contents of a trajectory file."
+        "traj",
+        help="View contents of a trajectory file.",
+        description="MDANSE stores trajectories as binary HDF5 files (.mdt). "
+        "This command allows you to view the contents of a trajectory file.",
     )
-    analysis = subparsers.add_parser("analysis")
-    param_group = parser.add_argument_group("Input parameters")
+    trajectory.add_argument(
+        "file_name", help="Path to the trajectory file, e.g. converted_dlpoly_run.mdt"
+    )
+    trajectory.set_defaults(func=show_trajectory_contents)
+    # Set up results options.
+    results = subparsers.add_parser(
+        "results",
+        help="View contents of a result file.",
+        description="This command allows you to check what analysis types are available in MDANSE "
+        "and to create analysis scripts for a given analysis type and trajectory file.",
+    )
+    results.add_argument(
+        "file_name", help="Path to the results file, e.g. dcsf_h2o_200K.mda"
+    )
+    results.set_defaults(func=show_trajectory_contents)
+    # Set up converter options.
+    converter = subparsers.add_parser(
+        "converter",
+        help="Create a script to convert MD output into an MDANSE .mdt file.",
+    )
+    converter.add_argument("-l", "--list", help="List all the converter types.")
+    converter.add_argument(
+        "-n", "--name", help="Name of the specific converter to be used."
+    )
+    converter.add_argument(
+        "-o", "--output", help="Use this file name for the output Python script."
+    )
+    # Set up analysis options.
+    analysis = subparsers.add_parser(
+        "analysis",
+        help="Create a script to analyse an MD trajectory.",
+        description="This command allows you to check what analysis types are available in MDANSE "
+        "and to create analysis scripts for a given analysis type and trajectory file.",
+    )
+    analysis.add_argument("-l", "--list", help="List all the analysis types.")
+    analysis.add_argument(
+        "-n", "--name", help="Name of the specific analysis to be used."
+    )
+    analysis.add_argument(
+        "-t", "--traj", help="Use this trajectory file as analysis input."
+    )
+    analysis.add_argument(
+        "-o", "--output", help="Use this file name for the output Python script."
+    )
+    # Everything has been set up.
+    return parser
 
-    parser.parse_args()
+
+def main():
+    parser = build_parsers()
+
+    args: Namespace = parser.parse_args()
+    if not vars(args):
+        parser.print_usage()
+    else:
+        args.func(args)
 
 
 if __name__ == "__main__":
