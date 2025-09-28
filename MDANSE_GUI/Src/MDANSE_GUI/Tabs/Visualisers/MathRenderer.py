0fee8230eb3705abff11c0e3edce85adbc261215
RobBuchananCompPhys <robertbb269@hotmail.com>
2025-09-28 10:13:30 +0100

    refactor to catch block math as well as inline

--- MDANSE_GUI/Src/MDANSE_GUI/Tabs/Visualisers/MathRenderer.py
+++ MDANSE_GUI/Src/MDANSE_GUI/Tabs/Visualisers/MathRenderer.py
@@ -25,33 +25,84 @@ class MathRenderer:
     def ignore(text: str) -> bool:
         return text in MathRenderer.ignores
 
+    @staticmethod
+    def containsMultiLineBlockExpression(text: str) -> bool:
+        return text == ".. math::" or text == r"<br />.. math::<br />"
+
+    @staticmethod
+    def containsBlockExpression(text: str) -> bool:
+        return text.startswith(".. math:")
+
+    @staticmethod
+    def containsInlineExpressions(text: str) -> bool:
+        pattern = r"(:math:`.*?`)"
+        substrings = re.split(pattern, text)
+        return len(substrings) > 1
+
+    @staticmethod
+    def processBlockExpression(text: str) -> list[tuple[str, bool]]:
+        return [(text[len(".. math:: "):], True)]
+
+    @staticmethod
+    def processMultiLineBlockExpression(strings: list[str], n: int) -> tuple[list[tuple[str, bool]], int]:
+        substrings = strings[n+1:]
+        group = []
+        for s in substrings:
+            if (not s) or (s == r"<br /><br />"):
+                break
+            group.append(s)
+
+        group = [s.replace(r"<br />", "") for s in group]
+        total = "".join(group)
+        if total.startswith(r"<br />") and total.endswith(r"<br />"):
+            result = total.split(r"<br />")[1]
+        else:
+            result = total.split(r"<br /><br />")[0]
+
+        return [(result, True)], n + len(group) + 1
+
+    @staticmethod
+    def processInlineExpressions(text: str) -> list[tuple[str, bool]]:
+        pattern = r"(:math:`.*?`)"
+        substrings = re.split(pattern, text)
+        scanned = []
+        for s in substrings:
+            if s.startswith(":math:"):
+                expr = s[len(":math:`"):-1]
+                scanned.append((expr, True))
+            else:
+                scanned.append((s, False))
+
+        return scanned
+
     def scan(self) -> list[tuple[str, bool]]:
         # Use regex matching to find expressions
-        pattern = r"(:math:`.*?`)"
+        pattern = r"(<br />.*?<br />)"
         substrings = re.split(pattern, self.raw_text)
 
         scanned = []
-        for s in substrings:
-            if not s:
-                # This is not a string - skip
-                continue
-
-            # Get the raw expression content to check if it is ignored
-            raw_expr = s[len(":math:") :]
-            if self.ignore(raw_expr):
-                # We ignore rendering this
-                prefix = MathRenderer.replace_ignored(raw_expr)
-                substrings[-1] = prefix + substrings[-1]
-                continue
-
-            if s.startswith(":math:`") and s.endswith("`"):
-                # This is a raw LaTex expression string - instantiate object to identify it as such
-                expr = s[len(":math:`") : -1]
-                scanned.append((expr, True))
-            else:
-                # Normal text string
-                scanned.append((s, False))
-
+        for index, s in enumerate(substrings):
+            if s:
+                if self.containsMultiLineBlockExpression(s):
+                    # Html is a multiline block expression
+                    group, end = self.processMultiLineBlockExpression(substrings, index)
+                    scanned.extend(group)
+                    substrings[index:end] = [""] * (end - index)
+                elif self.containsBlockExpression(s):
+                    # Html substring contains a block expression
+                    group = self.processBlockExpression(s)
+                    scanned.extend(group)
+                elif self.containsInlineExpressions(s):
+                    # Html substring contains inline math expressions
+                    group = self.processInlineExpressions(s)
+                    scanned.extend(group)
+                elif ":Example:" in s:
+                    # We have reached the end of the section containing math expressions
+                    rest = "".join(substrings[index:])
+                    scanned.append((rest, False))
+                else:
+                    # Html substring contains plain text only
+                    scanned.append((s, False))
         return scanned
 
     @staticmethod
