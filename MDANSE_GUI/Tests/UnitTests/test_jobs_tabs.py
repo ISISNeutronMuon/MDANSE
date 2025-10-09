481c69512fb4a3c825bbf6e1a97e4150dc6f3e40
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2025-10-09 16:54:40 +0100

    Apply suggestion from @oerc0122
    Signed-off-by: Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>

--- MDANSE_GUI/Tests/UnitTests/test_jobs_tabs.py
+++ MDANSE_GUI/Tests/UnitTests/test_jobs_tabs.py
@@ -76,6 +76,11 @@ def test_converter_widgets_load(qapp, qtbot, caplog, index):
     "index", enumerate(sorted(ENABLED_JOBS), 1), ids=lambda x: x[1]
 )
 def test_job_widgets_load(qapp, qtbot, caplog, index):
+    """
+    Test there are no major errors in constructing job widgets.
+    
+    This includes raises in the construction of widgets and missing widgets.
+    """
     window = QMainWindow()
 
     widget = JobTab.gui_instance(
