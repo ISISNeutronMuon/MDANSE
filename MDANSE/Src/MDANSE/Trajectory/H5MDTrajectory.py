466e6699c57679641641ab6de98947b9065dddfb
chi cheng <chi-yang.cheng@proton.me>
2025-10-21 17:32:49 +0100

    fix tests

--- MDANSE/Src/MDANSE/Trajectory/H5MDTrajectory.py
+++ MDANSE/Src/MDANSE/Trajectory/H5MDTrajectory.py
@@ -144,7 +144,6 @@ class H5MDTrajectory(TrajectoryFile):
 
         self._h5_file = h5py.File(self._h5_filename, "r")
 
-        self._load_unit_cells()
 
         particle_types = self._h5_file["/particles/all/species"]
         particle_lookup = h5py.check_enum_dtype(
@@ -177,14 +176,15 @@ class H5MDTrajectory(TrajectoryFile):
             )
             return
 
+        self._variables_to_skip = set()
+        self._load_units()
+        self._load_unit_cells()
+
         if self._chemical_system.rdkit_mol.GetNumBonds() > 0:
             configuration = self.configuration(0)
             contiguous_configuration = configuration.contiguous_configuration()
             coords = contiguous_configuration.coordinates
             self._chemical_system.set_bond_orders(coords)
-
-        self._variables_to_skip = set()
-        self._load_units()
 
     def _load_units(self) -> None:
         """Load units from h5 file."""
