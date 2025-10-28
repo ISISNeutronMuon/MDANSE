2e7ab4fbb5d977b446c0b6a647ed8b5abec36bcc
chi cheng <chi-yang.cheng@proton.me>
2025-10-28 10:24:12 +0000

    update documentation and warning log

--- MDANSE/Src/MDANSE/Chemistry/ChemicalSystem.py
+++ MDANSE/Src/MDANSE/Chemistry/ChemicalSystem.py
@@ -164,8 +164,11 @@ class ChemicalSystem:
                         f"Number of atoms in {cluster_name} with idx {idx} "
                         f"is very large - skipping bond type determination. "
                         f"SMARTS pattern matching will not work as expected. "
-                        f"Bond types set to UNSPECIFIED use bond type wildcard "
-                        f"~ to match bonds in this molecule."
+                        f"Bond types set to UNSPECIFIED, use the bond type wildcard "
+                        f"~ to match bonds in this molecule and use general atom symbols"
+                        f"e.g. [#6] instead of [C] or [c]. See "
+                        f"https://www.daylight.com/dayhtml/doc/theory/theory.smarts.html "
+                        f"for more details on SMARTS pattern matching."
                     )
                     continue
 
