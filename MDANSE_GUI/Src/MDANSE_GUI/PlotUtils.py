f7d4d6ecf43e3d1706ad937e096081b9591cc398
chi <chi-yang.cheng@proton.me>
2025-11-12 15:25:15 +0000

    improve MDANSEMatPlotLibNavBar

--- MDANSE_GUI/Src/MDANSE_GUI/PlotUtils.py
+++ MDANSE_GUI/Src/MDANSE_GUI/PlotUtils.py
@@ -16,16 +16,36 @@
 from __future__ import annotations
 
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT
+from qtpy.QtCore import QEvent, Qt
 from qtpy.QtGui import QIcon, QPixmap
+from qtpy.QtWidgets import QApplication
 
 
 class MDANSEMatPlotLibNavBar(NavigationToolbar2QT):
+    def __init__(self, canvas, parent=None, coordinates=True):
+        super().__init__(canvas, parent, coordinates)
+        colour_scheme = QApplication.styleHints().colorScheme()
+        self.icons_dark = []
+        self.icons_light = []
+        for action in self._actions.values():
+            prev_icon = action.icon()
+            pixmap = prev_icon.pixmap(24, 24)
+            img = pixmap.toImage()
+            img.invertPixels()
+            new_icon = QIcon(QPixmap.fromImage(img))
+            if colour_scheme == Qt.ColorScheme.Dark:
+                dark_icon, light_icon = prev_icon, new_icon
+            else:
+                light_icon, dark_icon = prev_icon, new_icon
+            self.icons_dark.append(dark_icon)
+            self.icons_light.append(light_icon)
+
     def changeEvent(self, event):
-        if event.type() == event.PaletteChange:
-            for action in self._actions.values():
-                pixmap = action.icon().pixmap(24, 24)
-                img = pixmap.toImage()
-                img.invertPixels()
-                new_pixmap = QPixmap.fromImage(img)
-                action.setIcon(QIcon(new_pixmap))
+        if event.type() == QEvent.PaletteChange:
+            colour_scheme = QApplication.styleHints().colorScheme()
+            for i, action in enumerate(self._actions.values()):
+                if colour_scheme == Qt.ColorScheme.Dark:
+                    action.setIcon(self.icons_dark[i])
+                else:
+                    action.setIcon(self.icons_light[i])
         super().changeEvent(event)
