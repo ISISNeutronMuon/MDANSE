eb713eb735666bd55a48a0ff7d1a14ee4f4c7294
RobBuchananCompPhys <robertbb269@hotmail.com>
2025-11-25 20:37:58 +0000

    use stem

--- MDANSE/Src/MDANSE/Framework/Formats/HDFFormat.py
+++ MDANSE/Src/MDANSE/Framework/Formats/HDFFormat.py
@@ -182,7 +182,7 @@ class HDFFormat(IFormat):
 
         else:
             filepath = Path(filename)
-            if "." in filepath.name:
+            if "." in filepath.stem:
                 filename = filepath.with_name(filepath.name + extension)
             else:
                 filename = filepath.with_suffix(extension)
