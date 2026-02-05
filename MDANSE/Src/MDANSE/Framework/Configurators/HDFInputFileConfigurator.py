fb84c10dd7250e8f3a3c571f52c7ec7a71717cfa
Maciej Bartkowiak <maciej.bartkowiak@stfc.ac.uk>
2026-02-05 10:49:14 +0000

    Fix several failing unit tests

--- MDANSE/Src/MDANSE/Framework/Configurators/HDFInputFileConfigurator.py
+++ MDANSE/Src/MDANSE/Framework/Configurators/HDFInputFileConfigurator.py
@@ -73,8 +73,6 @@ class HDFInputFileConfigurator(InputFileConfigurator):
                 f"Can not open {value} HDF file for reading: {traceback.format_exc()}"
             )
             return
-        else:
-            self["instance"] = None
 
         for v in self.variables:
             if v in self["instance"]:
