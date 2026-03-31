2986b1e444afdd6fe3571103a31bd913349f5067
chi cheng <chi-yang.cheng@proton.me>
2026-03-31 17:18:15 +0100

    implement basic farthest point sampling instead of using fpsample

--- MDANSE/Src/MDANSE/Framework/QVectors/SphericalLatticeQVectors.py
+++ MDANSE/Src/MDANSE/Framework/QVectors/SphericalLatticeQVectors.py
@@ -15,7 +15,6 @@
 #
 from __future__ import annotations
 
-import fpsample
 import numpy as np
 from scipy.spatial import KDTree
 
@@ -83,9 +82,8 @@ class SphericalLatticeQVectors(LatticeQVectors):
             lattice_hkl_vectors = lattice_hkl_vectors.T[selection].T
             q_vectors = self.hkl_to_qvectors(lattice_hkl_vectors, self._unit_cell)
 
-            if q_vectors.shape[1] > nvecs_per_shell:
-                selection = fpsample.fps_sampling(q_vectors.T, nvecs_per_shell)
-                q_vectors = q_vectors.T[selection].T
+            selection = fpsampling(q_vectors.T, nvecs_per_shell)
+            q_vectors = q_vectors.T[selection].T
 
             if self._configuration["force_equal_weights"]["value"]:
                 weights = np.ones(q_vectors.shape[1])
@@ -107,3 +105,46 @@ class SphericalLatticeQVectors(LatticeQVectors):
                 if self._status.is_stopped():
                     return
                 self._status.update()
+
+
+def fpsampling(q_vectors: np.ndarray, n_vecs: int) -> np.ndarray:
+    """Basic farthest point sampling function used to sample q-vectors.
+
+    Parameters
+    ----------
+    q_vectors : np.ndarray
+        Array of q_vectors.
+    n_vecs : int
+        Number of vectors to sample.
+
+    Returns
+    -------
+    np.ndarray
+        Index of selected q-vectors.
+
+    Raises
+    ------
+    ValueError
+        When `n_vecs` is zero or less than zero.
+    """
+    n_points = q_vectors.shape[0]
+    if n_vecs >= n_points:
+        return np.arange(n_points)
+    elif n_points == 1:
+        return np.array([np.random.randint(0, n_points)])
+    elif n_points <= 0:
+        raise ValueError("n_vecs should be greater than zero.")
+
+    dists = np.full(n_points, np.inf)
+    selected = np.random.randint(n_points)
+    selection = np.zeros(n_vecs, dtype=int)
+    selection[0] = selected
+
+    for i in range(1, n_vecs):
+        diff = q_vectors - q_vectors[selected]
+        dist_sq = np.sum(diff**2, axis=1)
+        dists = np.minimum(dists, dist_sq)
+        selected = np.argmax(dists)
+        selection[i] = selected
+
+    return selection
