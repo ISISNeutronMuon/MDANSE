7ce3975933475af2c5b445d34a0294380cd6413a
chi cheng <chi-yang.cheng@proton.me>
2026-05-19 16:14:36 +0100

    speed up for NPT

--- MDANSE/Src/MDANSE/Framework/Jobs/StaticStructureFactor3D.py
+++ MDANSE/Src/MDANSE/Framework/Jobs/StaticStructureFactor3D.py
@@ -101,7 +101,7 @@ class StaticStructureFactor3D(IJob):
         self.max = np.array([max_qx, max_qy, max_qz], dtype=np.float64)
         self.spacing = np.array([spacing_qx, spacing_qy, spacing_qz])
 
-        self._outputData.add("origin", "LineOutputVariable", self.min, units="nm")
+        self._outputData.add("origin", "LineOutputVariable", self.min, units="1/nm")
 
         self.gdim = np.ceil(np.array([dim_qx, dim_qy, dim_qz]) / self.spacing).astype(
             int
@@ -110,7 +110,7 @@ class StaticStructureFactor3D(IJob):
             "spacing",
             "LineOutputVariable",
             self.spacing,
-            units="nm",
+            units="1/nm",
         )
         self.grid = np.zeros(self.gdim, dtype=float)
 
@@ -124,7 +124,7 @@ class StaticStructureFactor3D(IJob):
                 "LineOutputVariable",
                 np.arange(0, self.gdim[naxis], 1) * self.spacing[naxis]
                 + self.min[naxis],
-                units="nm",
+                units="1/nm",
             )
 
         # TODO static structure factor between different element types
@@ -136,28 +136,29 @@ class StaticStructureFactor3D(IJob):
             main_result=True,
         )
 
+        # TODO if NPT then we should do this per run step
+        unit_cell = self.trajectory.unit_cell(0)
+        self.q_vectors = q_vectors_in_cube(
+            unit_cell.direct, unit_cell.inverse, self.min, self.max
+        )
+        self.q_idxs = np.floor((self.q_vectors.T - self.min) / self.spacing).astype(int)
+
     def run_step(self, index):
         traj = self.trajectory
 
         frame = self.configuration["frames"]["value"][index]
         coords = traj.configuration(frame)["coordinates"]
 
-        unit_cell = self.trajectory.unit_cell(frame)
-        q_vectors = q_vectors_in_cube(
-            unit_cell.direct, unit_cell.inverse, self.min, self.max
-        )
-        q_idxs = np.floor((q_vectors.T - self.min) / self.spacing).astype(int)
-
         rho = {}
         for element, idxs in self._indicesPerElement.items():
             selectedCoordinates = np.take(coords, idxs, axis=0)
             rho[element] = np.sum(
-                np.exp(1j * np.dot(selectedCoordinates, q_vectors)), axis=0
+                np.exp(1j * np.dot(selectedCoordinates, self.q_vectors)), axis=0
             )
 
         grid = np.zeros(self.gdim)
         for _, (label_i, label_j) in self.labels:
-            np.add.at(grid, tuple(q_idxs.T), (rho[label_i] * rho[label_j].conj()).real)
+            np.add.at(grid, tuple(self.q_idxs.T), (rho[label_i] * rho[label_j].conj()).real)
         return index, grid
 
     def combine(self, index, x):
