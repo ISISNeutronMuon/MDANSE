abf723aca33934a46e06b0d49ce8e2de8df6b9f4
chi <chi-yang.cheng@proton.me>
2026-08-19 16:28:00 +0100

    ruff

--- MDANSE/Src/MDANSE/Framework/Jobs/GaussianDynamicIncoherentStructureFactor.py
+++ MDANSE/Src/MDANSE/Framework/Jobs/GaussianDynamicIncoherentStructureFactor.py
@@ -24,8 +24,8 @@ from MDANSE.Framework.Jobs.IJob import IJob
 from MDANSE.Mathematics.Arithmetic import assign_weights, get_weights, weighted_sum
 from MDANSE.Mathematics.Signal import get_spectrum
 from MDANSE.MolecularDynamics.Analysis import mean_square_displacement
-from MDANSE.util_types import FloatArray
 from MDANSE.MolecularDynamics.TrajectoryUtils import group_atom_indices
+from MDANSE.util_types import FloatArray
 
 
 @IJob.register("GaussianDynamicIncoherentStructureFactor")
